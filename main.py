834af4ad58d5a9af966aa268495204390906e38c
unknown <pawhal@st.amu.edu.pl>
2017-10-13 21:55:53 +0200

    bla

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from exercise import Exercise
-temp_exercise = Exercise(1, "dupa")
+temp_exercise = Exercise(12, 'costam')
 temp_exercise.number = int(input())
 temp_exercise.query = input()
 temp_exercise.show()
