ad8a2b45ac74c43c18619dbeea7dc032ff2120bb
pawhal <pawhal@st.amu.edu.pl>
2017-10-26 19:36:41 +0200

    better scheduler

--- main.py
+++ main.py
@@ -1,40 +1,14 @@
 from stuff import *
-import schedule
-import time
-from multiprocessing import Process, Manager
+from apscheduler.schedulers.background import BackgroundScheduler
 
+base = {}
 
-def save(file, d):
-    print(file)
-    print(base)
-    with open(file, "w+") as f:
-        for key, val in sorted(d.items()):
-             f.write(str(key) + " " + val + "\n")
-    print("saved")
+sch = BackgroundScheduler()
+sch.start()
 
+sch.add_job(save, "cron", args = ("/home/paw/projekt1_2/odp.txt", base), second='*/30')
+sch.add_job(classes, "cron", minute = '*/1')
+sch.add_job(breaks, "cron", minute = '*/1')
 
-def inp(base):
-    while 1:
-        temp_exercise = Exercise(int(input()), input())
-        if temp_exercise.valid():
-            base[temp_exercise.number] = temp_exercise.query
-            print("bangla")
-        else:
-            print("nie bangla")
-
-
-def f1(d):
-    schedule.every(30).seconds.do(save, "/home/paw/projekt1_2/odp.txt", d)
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
-
-
-if __name__=='__main__':
-    manager = Manager()
-    base = Manager().dict()
-    p1 = Process(target=f1, args=(base,))
-    p1.start()
-    p1.join()
-    while 1:
-        inp(base)
+while 1:
+    inp(base)
--- stuff.py
+++ stuff.py
@@ -13,6 +13,27 @@ class Exercise:
             return False
 
 
+def save(file, d):
+    print(file)
+    print(d)
+    with open(file, "w+") as f:
+        for key, val in sorted(d.items()):
+             f.write(str(key) + " " + val + "\n")
+    print("saved")
 
 
+def inp(d):
+    while 1:
+        temp_exercise = Exercise(int(input()), input())
+        if temp_exercise.valid():
+            d[temp_exercise.number] = temp_exercise.query
+            print("bangla")
+        else:
+            print("nie bangla")
 
+def classes():
+    print("minut do konca zajec")
+
+
+def breaks():
+    print("minut do konca przerwy")
